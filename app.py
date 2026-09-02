f32a71d9d3bba8c675378ff4b83ccc036cdc25d8
radubb223 <barzanradu23@gmail.com>
2026-09-02 11:31:31 +0300

    dockerize and adding compose for the app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.secret_key = 'flash message'
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = 'mysql'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'crud_flask_app'
@@ -72,6 +72,25 @@ def delete(id_data):
     mysql.connection.commit()
     return redirect(url_for('index'))
 
+def init_db():
+    with app.app_context():
+        cur = mysql.connection.cursor()
+
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS students (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(100) NOT NULL,
+                email VARCHAR(100) NOT NULL,
+                phone VARCHAR(20),
+                address VARCHAR(255),
+                city VARCHAR(100),
+                pincode VARCHAR(20)
+            )
+        """)
+
+        mysql.connection.commit()
+        cur.close()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    init_db()
+    app.run(host="0.0.0.0", port=5000)
